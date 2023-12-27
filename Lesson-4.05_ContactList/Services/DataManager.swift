c87272634129d531b9c5bc43b71fd66f06039393
Iurii Kurinnoi <yur4kur@icloud.com>
2023-12-27 09:37:25 +0400

    DataStore has been added.

--- Lesson-4.05_ContactList/Services/DataManager.swift
+++ Lesson-4.05_ContactList/Services/DataManager.swift
@@ -8,62 +8,9 @@
 // MARK: - DataManager
 final class DataManager {
     
-    // MARK: Singleton property
+    // MARK: - Public properties
+    
     static let shared = DataManager()
-    
-    // MARK: - Private properties
-    
-    // MARK: Names
-    private let names = [
-        "Leanne",
-        "Ervin",
-        "Clementine",
-        "Patricia",
-        "Chelsey",
-        "Dennis",
-        "Kurtis",
-        "Nicholas",
-        "Glenna",
-        "Clementina"]
-    
-    // MARK: Surnames
-    private let surnames = [
-        "DuBuque",
-        "Reichert",
-        "Runolfsdottir",
-        "Weissnat",
-        "Schulist",
-        "Dietrich",
-        "Lebsack",
-        "Bauch",
-        "Howell",
-        "Graham"]
-    
-    // MARK: Phone numbers
-    private let phoneNumbers = [
-        "1-770-736-8031",
-        "010-692-6593",
-        "1-463-123-4447",
-        "1-493-170-9623",
-        "44(254)954-1289",
-        "1-477-935-8478",
-        "1-210-067-6132",
-        "1-586-493-6943",
-        "44(775)976-6794",
-        "1-024-648-3804"]
-    
-    // MARK: Emails
-    private let emails = [
-        "Rey.Padberg@karina.biz",
-        "Chaim_McDermott@dana.io",
-        "Sherwood@rosamond.me",
-        "Telly.Hoeger@billy.biz",
-        "Karley_Dach@jasper.info",
-        "Lucio_Hettinger@annie.ca",
-        "Julianne.OConner@kory.org",
-        "Nathan@yesenia.net",
-        "Shanna@melissa.tv",
-        "Sincere@april.biz"]
     
     // MARK: - Initializers
     
@@ -71,13 +18,13 @@ final class DataManager {
     
     // MARK: - Public methods
     
-    static func getPersons() -> [Person] {
+    class func getPersons() -> [Person] {
         var contactList: [Person] = []
         
-        let names = DataManager.shared.names.shuffled()
-        let surnames = DataManager.shared.surnames.shuffled()
-        let phones = DataManager.shared.phoneNumbers.shuffled()
-        let emails = DataManager.shared.emails.shuffled()
+        let names = DataStore.shared.names.shuffled()
+        let surnames = DataStore.shared.surnames.shuffled()
+        let phones = DataStore.shared.phoneNumbers.shuffled()
+        let emails = DataStore.shared.emails.shuffled()
         
         let iterationCount = min(
             names.count,
